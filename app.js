53ebd7332d7b2788c256759f7ec06a25def94bcf
Mathzzuco <matheusmazzuco05@gmail.com>
2025-06-10 13:43:46 -0300

    initial files and display messages

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const path = require("node:path");
+
+const indexRouter = require("./routes/indexRouter");
+const newRouter = require("./routes/newRouter");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use("/", indexRouter);
+app.use("/new", newRouter);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).send(err.message);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`My first Express app - listening on port ${PORT}!`);
+});
